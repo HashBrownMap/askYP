45f77885933d81f5c71e8859a9131df31f5ebb48
Tony Chen <t.z.chen96@gmail.com>
2017-01-22 10:30:36 -0500

    working

--- bot.py
+++ bot.py
@@ -3,11 +3,13 @@
 import os
 import tweepy, time, sys
 from secrets import * 
-
+from nltk.tag import pos_tag
+import string
 from tweepy import Stream
 from tweepy.streaming import StreamListener
-from HTMLParser import HTMLParser
 import json
+import urllib
+
 
 class StreamListener(tweepy.StreamListener):
 	print "yo"
@@ -17,20 +19,54 @@ class StreamListener(tweepy.StreamListener):
 			print "Hello World2"
 			data = {}
 			location = ""
-			data[status.id] = []
+			exclude = set('!?.,')
 
 			location = status.place
-			data[status.d].append({
-				'tweet': status.text,
-				'user': status.user,
-				'location': location
-				})
-			with open('questions.json', 'a') as f: 
-				json.dump(data, f)
+
+			#with open('questions.json', 'a') as f: 
+				
+			print status.text
+			print status.id
+			print status.user.screen_name
+
+			
+
 			time.sleep(5)
+
+			sentence = status.text.replace('#askYP', '')
+			sentence = ''.join(ch for ch in sentence if ch not in exclude)
+			tagged_sent = pos_tag(sentence.split())
+
+			targets = [word for word, pos in tagged_sent if pos == 'NN' or pos == 'NNP']
+			
+
+			reponse = []
+			collection = []
+			try:
+				for i in targets:
+					url = 'http://hackaton.ypcloud.io/search?what=%s&longtitude=-73.516388&latitue=45.522554' % i
+					response.append(urllib.urlopen(url))
+			except BaseException as e:
+				print "ERROR"
+				exit(1)
+
+			for j in response:
+				collection.append(json.loads(response[j].read()))
 			#with open('questions.json', 'r') as r:
 			#	json_decode = json.load(r)
-			api.update_status('@%s My status update' % (status.user), status.id)
+			
+			stores = []
+			location = []
+
+			for info in collection:
+				stores += info["merchants"]["businessName"]
+				location +=info["address"]["displayLine"]
+			
+
+			api.update_status('@%s you want %s. You can find it at %s at %s' % (status.user.screen_name, targets[0], stores[0], location[0]), status.id)
+			time.sleep(2)
+			#api.update_status('next one yo')
+			return True
 		except BaseException as e:
 			print("Error on_data: %s" % str(e))
 		return True
@@ -44,8 +80,6 @@ if __name__ == '__main__':
 	auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 	auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 	api = tweepy.API(auth)
-	print "Hello World"
-
 	MyStreamListener = StreamListener()
 	twitter_stream = Stream(auth, MyStreamListener)
 	twitter_stream.filter(track=['#askUUU'])
