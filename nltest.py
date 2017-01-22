8cb69d55d375151cebeaa919def5260ebd4b974c
Tony Chen <t.z.chen96@gmail.com>
2017-01-22 02:50:24 -0500

    Add files via upload

--- nltest.py
+++ nltest.py
@@ -1,7 +1,17 @@
-import spacy
-nlp = spacy.load('en')
-doc = nlp(u'London is a big city in the United Kingdom.')
-print "lmao"
-for ent in doc.ents:
-    print "lol"
-    print(ent.label_, ent.text)
+from nltk.tag import pos_tag
+import string
+
+sentence = "Where can I find McDonald's?"
+exclude = set('!?.,')
+sentence = ''.join(ch for ch in sentence if ch not in exclude)
+tagged_sent = pos_tag(sentence.split())
+print tagged_sent
+
+propernouns = [word for word, pos in tagged_sent if pos == 'NN' or pos == 'NNP']
+print propernouns 
+
+#take tweet
+#exclude 
+#tag/split
+#propernouns
+#search each proper noun
