2beb2d9a164241f18139540f74916c3bae6004af
Zoe Hu <zoe.hu@mail.mcgill.ca>
2017-01-22 02:53:29 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -17,20 +17,22 @@ class StreamListener(tweepy.StreamListener):
 			print "Hello World2"
 			data = {}
 			location = ""
-			data[status.id] = []
 
 			location = status.place
-			data[status.d].append({
-				'tweet': status.text,
-				'user': status.user,
-				'location': location
-				})
+
 			with open('questions.json', 'a') as f: 
-				json.dump(data, f)
+				
+				print status.text
+				print status.id
+				print status.user.screen_name
 			time.sleep(5)
 			#with open('questions.json', 'r') as r:
 			#	json_decode = json.load(r)
-			api.update_status('@%s My status update' % (status.user), status.id)
+			
+			api.update_status('@%s nice tweet bro' % status.user.screen_name, status.id)
+			time.sleep(2)
+			api.update_status('next one yo')
+			return True
 		except BaseException as e:
 			print("Error on_data: %s" % str(e))
 		return True
@@ -44,8 +46,6 @@ if __name__ == '__main__':
 	auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 	auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 	api = tweepy.API(auth)
-	print "Hello World"
-
 	MyStreamListener = StreamListener()
 	twitter_stream = Stream(auth, MyStreamListener)
 	twitter_stream.filter(track=['#askUUU'])
